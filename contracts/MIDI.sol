f86f9b4a67df0175b9f833ab3c7b79a3574cf54d
Stuart Kuentzel <s.w.kuentzel@gmail.com>
2021-08-02 23:48:16 +0900

    removes unused contract functions

--- contracts/MIDI.sol
+++ contracts/MIDI.sol
@@ -1,6 +1,5 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
-// import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
@@ -44,23 +43,11 @@ contract MIDI is ERC721Enumerable, Ownable {
         return marketContractAddress;
     }
 
-    // mapping(uint256 => uint256) patchToDevice;
-    // mapping(uint256 => address) patchToOwner;
-
     function createPatch(
         string memory _patchName,
         uint256 _deviceId,
         uint8[] memory _data
-    )
-        external
-        returns (
-            // bytes calldata _data,
-            // bytes memory _data
-            uint256
-        )
-    {
-        // console.log(_data);
-        // console.log("hello world", string(_data));
+    ) external returns (uint256) {
         require(
             devices.length > 0 && (devices.length - 1 >= _deviceId),
             "No Device ID found"
@@ -69,7 +56,6 @@ contract MIDI is ERC721Enumerable, Ownable {
         uint256 newPatchId = _patchIds.current();
         _safeMint(msg.sender, newPatchId);
 
-        // patches.push(Patch(_data, _patchName, _deviceId));
         tokenIdToPatch[newPatchId] = Patch(_data, _patchName, _deviceId);
         devicePatchCount[_deviceId]++;
 
@@ -88,8 +74,6 @@ contract MIDI is ERC721Enumerable, Ownable {
     }
 
     function fetchPatch(uint256 _tokenId) public view returns (Patch memory) {
-        // require(patches[_tokenId].isPublic || ownerOf(_tokenId) == msg.sender);
-        // return patches[_tokenId];
         return tokenIdToPatch[_tokenId];
     }
 
@@ -118,38 +102,6 @@ contract MIDI is ERC721Enumerable, Ownable {
         return devicePatchCount[_deviceId];
     }
 
-    // function getPatchesByDeviceId(uint256 _deviceId)
-    //     public
-    //     view
-    //     returns (Patch[] memory)
-    // {
-    //     Patch[] memory devicePatches;
-    //     uint256 counter = 0;
-    //     for (uint256 i = 0; i < patches.length; i++) {
-    //         if (patches[i].deviceId == _deviceId) {
-    //             devicePatches[counter] = patches[i];
-    //             counter++;
-    //         }
-    //     }
-    //     return devicePatches;
-    // }
-
-    // function getPatchesByOwner(address _owner)
-    //     external
-    //     view
-    //     returns (uint256[] memory)
-    // {
-    //     uint256[] memory result = new uint256[](balanceOf(_owner));
-    //     uint256 counter = 0;
-    //     for (uint256 i = 1; i < patches.length; i++) {
-    //         if (ownerOf(i) == _owner) {
-    //             result[counter] = i;
-    //             counter++;
-    //         }
-    //     }
-    //     return result;
-    // }
-
     function getPatchesByOwner(address _owner)
         external
         view
@@ -163,20 +115,22 @@ contract MIDI is ERC721Enumerable, Ownable {
         return result;
     }
 
-    // function setPatchVisibility(uint256 _tokenId, bool _isPublic) public {
-    //     require(ownerOf(_tokenId) == msg.sender);
-    //     patches[_tokenId].isPublic = _isPublic;
-    // }
+    function getPatchDataByIds(uint256[] memory _tokenIds)
+        public
+        view
+        returns (uint8[][] memory)
+    {
+        uint8[][] memory results = new uint8[][](_tokenIds.length);
+
+        for (uint256 i = 0; i < _tokenIds.length; i++) {
+            Patch memory patch = tokenIdToPatch[_tokenIds[i]];
+            results[i] = patch.data;
+        }
+
+        return results;
+    }
 
     function getDevices() public view returns (Device[] memory) {
         return devices;
     }
-
-    // function transfer(
-    //     address from,
-    //     address to,
-    //     uint256 tokenId
-    // ) public {
-    //     safeTransferFrom(from, to, tokenId);
-    // }
 }
